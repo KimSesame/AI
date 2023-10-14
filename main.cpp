3ef1126ec8b5bae6c00d33644902ed282a55c7d7
김형진 <kimbs0827@naaver.com>
2023-10-14 17:38:51 +0900

    Add isTerminate, value, generateGameTree function

--- main.cpp
+++ main.cpp
@@ -13,7 +13,25 @@ using namespace std;
 
 #define MAX_PLY 3
 
+struct Node;
 class GameManager;
+
+int isTerminate(const vector<vector<int>> board);
+int value(const vector<vector<int>> board, int user);
+Node *generateGameTree(int depth, Node *parent = nullptr);
+
+// Game-Tree node
+struct Node
+{
+    vector<vector<int>> board;
+    Node *parent = nullptr;
+    vector<Node *> children;
+    int sx, sy, gx, gy; // last moved pawn's coordinate
+    int v;              // evaluation value
+
+    Node(vector<vector<int>> board, int sx, int sy, int gx, int gy)
+        : board(board), sx(sx), sy(sy), gx(gx), gy(gy) {}
+};
 
 class GameManager
 {
@@ -32,20 +50,24 @@ public:
     void printBoard(void);
     int evaluate(void);
     void userMove(void);
+    void comMove(void);
     bool movePawn(int sx, int sy, int gx, int gy);
 
     int get_user(void) const { return user; }
+    int get_com(void) const { return opponent; }
+    vector<vector<int>> get_board(void) const { return board; }
 };
+
+GameManager manager;
 
 int main(void)
 {
-    GameManager manager;
     int game_over = 0;
 
     // test///////////////////
     while (game_over == 0)
     {
-        game_over = manager.evaluate();
+        game_over = isTerminate(manager.get_board());
         manager.printBoard();
 
         if (game_over)
@@ -150,39 +172,16 @@ void GameManager::printBoard(void)
     return 0 if game in progress */
 int GameManager::evaluate(void)
 {
-    // Check if game end
-    // 1. No one's pawn on the board
-    // 2. One's pawn reached on the other side
-
-    // Check pawn existence
-    bool user_exist = false;
-    bool op_exist = false;
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (board[i][j] == user)
-                user_exist = true;
-            else if (board[i][j] == opponent)
-                op_exist = true;
-
-    // Check reached pawn
-    bool user_reached = false;
-    bool op_reached = false;
-
-    for (int j = 0; j < 3; j++)
-        if (board[(user + 1) % 3][j] == user)
-            user_reached = true;
-        else if (board[(opponent + 1) % 3][j] == opponent)
-            op_reached = true;
+    int winner = isTerminate(board);
 
     // Evaluate with ended game
-    if ((!op_exist) || user_reached) // user win
+    if (winner == user) // user win
     {
         value[user] = 100;
         value[opponent] = -100;
         return user;
     }
-    else if ((!user_exist) || op_reached) // opponent win
+    else if (winner == opponent) // opponent win
     {
         value[opponent] = 100;
         value[user] = -100;
@@ -265,6 +264,16 @@ void GameManager::userMove(void)
     return;
 }
 
+/*  Computer's move method
+    Get computer's move by min-max algorithm with Alpha-Beta pruing and update board state after move */
+void GameManager::comMove(void)
+{
+    Node *root = new Node(board, 0, 0, 0, 0);
+
+    // Generate tree
+    generateGameTree(MAX_PLY + 1);
+}
+
 /*  Move pawn method
     Input: start_x, start_y, goal_x, goal_y
     Move pawn at (start_x, start_y) to (goal_x, goal_y)
@@ -330,4 +339,171 @@ bool GameManager::movePawn(int sx, int sy, int gx, int gy)
     }
 
     return false;
+}
+
+/*  Check game is terminated
+    inspect the input game board
+    return 1 if WHITE win
+    return 2 if BLACK win
+    return 0 if game in progress */
+int isTerminate(const vector<vector<int>> board)
+{
+    // Check if game end
+    // 1. No one's pawn on the board
+    // 2. One's pawn reached on the other side
+
+    // Check pawn existence
+    bool w_exist = false;
+    bool b_exist = false;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] == WHITE)
+                w_exist = true;
+            else if (board[i][j] == BLACK)
+                b_exist = true;
+
+    // Check reached pawn
+    bool w_reached = false;
+    bool b_reached = false;
+
+    for (int j = 0; j < 3; j++)
+        if (board[2][j] == WHITE)
+            w_reached = true;
+        else if (board[0][j] == BLACK)
+            b_reached = true;
+
+    // Inspect winner
+    if ((!b_exist) || w_reached) // WHITE win
+        return WHITE;
+    else if ((!w_exist) || b_reached) // BLACK win
+        return BLACK;
+    else
+        return 0; // gaem in progress
+}
+
+/*  Return user's value of board state */
+int value(const vector<vector<int>> board, int user)
+{
+    int winner = isTerminate(board);
+    int v;
+
+    // Evaluate with ended game
+    if (winner)
+        if (winner == user) // user win
+            return 100;
+        else // opponent win
+            return -100;
+
+    // Evaluate with board in progress
+    int w_val = 0, b_val = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        w_val += count(board[i].begin(), board[i].end(), WHITE) * i * i;
+        w_val -= count(board[i].begin(), board[i].end(), BLACK) * (4 - i) * (4 - i);
+
+        b_val += count(board[i].begin(), board[i].end(), BLACK) * (2 - i) * (2 - i);
+        b_val -= count(board[i].begin(), board[i].end(), WHITE) * (i + 2) * (i + 2);
+    }
+
+    return (user == WHITE ? w_val : b_val);
+}
+
+/*  Generate game tree using DFS */
+Node *generateGameTree(int depth, Node *parent)
+{
+    Node *new_node;
+
+    if (depth == 0)
+        return nullptr;
+
+    // Generate node if possible move
+    if (parent == nullptr) // game tree root
+        new_node = new Node(manager.get_board(), 0, 0, 0, 0);
+    else
+    {
+        vector<vector<int>> board(parent->board);
+
+        for (int sx = 0; sx < 3; sx++)
+            for (int sy = 0; sy < 3; sy++)
+                // Computer's pawn selected
+                if (board[sx][sy] == manager.get_com())
+                    for (int gx = 0; gx < 3; gx++)
+                        for (int gy = 0; gy < 3; gy++)
+                            // Move white pawn
+                            if (board[sx][sy] == WHITE)
+                            {
+                                if ((abs(gx - sx) > 1) || (abs(gy - sy) > 1)) // too far
+                                {
+                                }
+                                else if (board[gx][gy] == WHITE) // blocked by white pawn
+                                {
+                                }
+                                // BLACK pawn exist
+                                else if (board[gx][gy] == BLACK)
+                                    if ((sx + 1 == gx) && (sy != gy)) // kill by diagonal move
+                                    {
+                                        board[gx][gy] = board[sx][sy];
+                                        board[sx][sy] = EMPTY;
+                                        new_node = new Node(board, sx, sy, gx, gy);
+                                    }
+                                    else // blocked by black pawn
+                                    {
+                                    }
+                                else if (board[gx][gy] == EMPTY) // destination is empty
+                                    if ((gx - sx) * (gy - sy) == 0)
+                                    {
+                                        board[gx][gy] = board[sx][sy];
+                                        board[sx][sy] = EMPTY;
+                                        new_node = new Node(board, sx, sy, gx, gy);
+                                    }
+                                    else // cannot diagonal move
+                                    {
+                                    }
+                            }
+                            // Move black pawn
+                            else if (board[sx][sy] == BLACK)
+                            {
+                                if ((abs(gx - sx) > 1) || (abs(gy - sy) > 1)) // too far
+                                {
+                                }
+                                else if (board[gx][gy] == BLACK) // blocked by black pawn
+                                {
+                                }
+                                // WHITE pawn exist
+                                else if (board[gx][gy] == WHITE)
+                                {
+                                    if ((sx - 1 == gx) && (sy != gy)) // kill by diagonal move
+                                    {
+                                        board[gx][gy] = board[sx][sy];
+                                        board[sx][sy] = EMPTY;
+                                        new_node = new Node(board, sx, sy, gx, gy);
+                                    }
+                                    else // blocked by white pawn
+                                    {
+                                    }
+                                }
+                                else if (board[gx][gy] == EMPTY) // destination is empty
+                                    if ((gx - sx) * (gy - sy) == 0)
+                                    {
+                                        board[gx][gy] = board[sx][sy];
+                                        board[sx][sy] = EMPTY;
+                                        new_node = new Node(board, sx, sy, gx, gy);
+                                    }
+                                    else // cannot diagonal move
+                                    {
+                                    }
+                            }
+    }
+
+    // Set parent-child connection
+    new_node->parent = parent;
+    new_node->children.push_back(generateGameTree(depth - 1, new_node));
+
+    // Terminal node
+    if (depth == 1)
+        new_node->v = value(new_node->board, manager.get_com()); // evaluate value
+
+    return new_node;
 }
